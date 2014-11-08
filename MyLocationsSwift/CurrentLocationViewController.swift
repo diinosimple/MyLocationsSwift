6c864cc62a832e1d4ecbaf79ebaa25eebb650297
Daisuke Iino <diinosimple@gmail.com>
2014-11-08 13:53:38 +0900

    finished p110

--- MyLocationsSwift/CurrentLocationViewController.swift
+++ MyLocationsSwift/CurrentLocationViewController.swift
@@ -249,5 +249,14 @@ class CurrentLocationViewController: UIViewController,CLLocationManagerDelegate{
             }
             
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "TagLocation" {
+            let navigationController = segue.destinationViewController as UINavigationController
+            let controller = navigationController.topViewController as LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
 }
 
