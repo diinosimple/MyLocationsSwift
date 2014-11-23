d57c8051e8f1e976168be9c7e5f66b0f24472499
Daisuke Iino <diinosimple@gmail.com>
2014-11-23 22:58:27 +0900

    Page 220

--- MyLocationsSwift/Location.swift
+++ MyLocationsSwift/Location.swift
@@ -9,8 +9,9 @@
 import Foundation
 import CoreData
 import CoreLocation
+import MapKit
 
-class Location: NSManagedObject {
+class Location: NSManagedObject, MKAnnotation {
 
     @NSManaged var latitude: Double
     @NSManaged var longitude: Double
@@ -18,5 +19,21 @@ class Location: NSManagedObject {
     @NSManaged var locationDesctiption: String
     @NSManaged var category: String
     @NSManaged var placemark: CLPlacemark?
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    var title: String! {
+        if locationDesctiption.isEmpty {
+            return "(No Description)"
+        } else {
+            return locationDesctiption
+        }
+    }
+    
+    var subtitle: String! {
+        return category
+    }
 
 }
